9d2fe2d0e75f01ec181ba0e23c0fc8512838e728
Francesco Caglioti <francesco.caglioti@outlook.com>
2023-10-20 19:48:43 +0200

    2.30 - PDO Part 1

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\View;
+use PDO;
+use PDOException;
 
 class HomeController
 {
@@ -42,15 +44,96 @@ class HomeController
 
         // return ( new View('index') )->render();
 
-        return (string) View::render('index', [ 'foo'=> 'bar' ]);
 
+        try {
+            $db = new PDO(
+                'mysql:host=localhost;dbname=my_db',
+                'root',
+                '',
+                [
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+                ]
+            );
+
+            // $query = "SELECT * FROM users";
+
+            // // $stt = $db->query( $query );
+            // // $result = $stt->fetchAll();
+            // // echo'<pre>';var_dump($result);die();
+
+            // foreach ($db->query($query) as $user) {
+            //     echo '<pre>';
+            //     var_dump( $user );
+            //     echo '</pre>';
+            // }
+
+
+            // $query = "SELECT * FROM users WHERE email = ?";
+            // $stt = $db->prepare($query);
+            // $stt->execute([$email]);
+
+            // foreach ($stt->fetchAll() as $user) {
+            //     echo '<pre>';
+            //     var_dump( $user );
+            //     echo '</pre>';
+            // }
+
+            // $query = "INSERT INTO users (email, full_name, is_active, created_at)
+            //             VALUES( ?,?,?,?)";
+
+
+            // $stt = $db->prepare($query);
+            // $stt->execute([$email, $name, $isActive, $createdAt]);
+
+            // $id = $db->lastInsertId();
+
+            // $user = $db->query( "SELECT * FROM users WHERE id = $id" )->fetch();
+
+            // echo'<pre>';var_dump($user);die();
+
+
+            $query = "INSERT INTO users (email, full_name, is_active, created_at)
+                        VALUES( :email, :full_name, :is_active, :created_at)";
+
+            $stt = $db->prepare($query);
+
+            $email = 'jonathan@doe.com';
+            $name = "Jonny Doe";
+            $isActive = 1;
+            $createdAt = date('Y-m-d H:i:s');
+
+            $stt->bindValue( ':email', $email );
+            $stt->bindValue( ':full_name', $name );
+            $stt->bindValue( ':is_active', $isActive, PDO::PARAM_BOOL );
+            $stt->bindValue( ':created_at', $createdAt );
+
+            $stt->execute();
+
+            $id = $db->lastInsertId();
+
+            $user = $db->query("SELECT * FROM users WHERE id = $id")->fetch();
+
+            echo '<pre>';
+            var_dump($user);
+            die();
+
+        } catch (\PDOException $e) {
+            throw new PDOException($e->getMessage(), (int) $e->getCode());
+        }
+
+        echo '<pre>';
+        var_dump($db);
+        die();
+
+
+        return (string) View::render('index', ['foo' => 'bar']);
     }
 
     public function download()
     {
         header('Content-Type: application/pdf');
         header('Content-Disposition: attachment;filename="myfile.pdf"');
-        readfile( STORAGE_PATH . '/FILE_NAME' );
+        readfile(STORAGE_PATH . '/FILE_NAME');
     }
 
     public function upload()
